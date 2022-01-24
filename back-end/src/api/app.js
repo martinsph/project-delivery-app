35bf5fd5836d0c2a92daa137b51a517ca2305f38
Diego Figueirêdo <8kball@gmail.com>
2022-01-24 19:15:27 -0300

    Cria estrutura MSC

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const root = require('../controllers/root');
 
 const app = express();
 
 app.get('/coffee', (_req, res) => res.status(418).end());
+
+app.use('/', root);
 
 module.exports = app;
