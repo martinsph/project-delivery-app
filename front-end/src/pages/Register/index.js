8cba512e939346f94eb74384d75a4ce069b9d6d6
Martin <escorpmartin97@gmail.com>
2022-01-25 19:36:24 -0300

    adiciona estilos e apaga arquivo de styles.js

--- front-end/src/pages/Register/index.js
+++ front-end/src/pages/Register/index.js
@@ -1,44 +1,88 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function Login() {
+const RegisterContent = styled.div`
+  width: 400px; 
+  height: 150px;
+  left: 50%; 
+  margin: -130px 0 0 -210px; 
+  padding: 10px;
+  position: absolute;
+  top: 20%;
+  display: flex;
+  justify-content: center;
+  
+  h2 {
+    display: flex;
+    justify-content: center;
+  }
+`;
+const FormRegister = styled.form`
+  background-color: #D3D3D3;
+  padding: 30px;
+  position: absolute;
+  top: 50%;
+  width: 50%;
+  `;
+const LabelRegister = styled.label`
+  margin: 15px;
+  `;
+
+const Button = styled.button`
+  padding: 5px 25px;
+  background-color: #008f39;
+  border: black;
+  color: white;
+  display: flex;
+  justify-content: center;
+`;
+
+const InputRegister = styled.input`
+  display: flex;
+  display: 
+  width: 100%;
+  padding: 10px;
+  margin: 10px
+`;
+
+function Register() {
   return (
-    <div className="register-content">
-      <form>
-        <label htmlFor="name">
+    <RegisterContent>
+      <h2>Cadastro</h2>
+      <FormRegister>
+        <LabelRegister htmlFor="name">
           Nome
-          <input
+          <InputRegister
             placeholder="Seu nome"
             data-testid="common_register__input-name"
             name="name"
           />
-        </label>
-
-        <label htmlFor="email">
+        </LabelRegister>
+        <LabelRegister htmlFor="email">
           Email
-          <input
+          <InputRegister
             placeholder="seu-email@site.com.brr"
             data-testid="common_register__input-email"
             name="email"
           />
-        </label>
-        <label htmlFor="password">
+        </LabelRegister>
+        <LabelRegister htmlFor="password">
           Senha
-          <input
+          <InputRegister
             placeholder="******"
             data-testid="common_register__input-password"
             name="password"
           />
-        </label>
-        <button
+        </LabelRegister>
+        <Button
           type="button"
           data-testid="common_register__button-register"
         >
           CADASTRAR
-        </button>
-      </form>
-    </div>
-
+        </Button>
+      </FormRegister>
+    </RegisterContent>
   );
 }
 
-export default Login;
+export default Register;
