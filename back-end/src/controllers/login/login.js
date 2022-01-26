706548af28452126be736a07ae5d4d390097b852
martinsph <ph.mmartins@gmail.com>
2022-01-26 14:19:34 -0300

    add backend login

--- back-end/src/controllers/login/login.js
+++ back-end/src/controllers/login/login.js
@@ -1,5 +1,14 @@
-const login = (req, res) => {
-  res.send('oi');
+const services = require('../../services/login');
+
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const result = await services({ email, password });
+    // console.log(result);
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = login;
