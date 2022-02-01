864473156f6beba77efea17269979dbcd4e7f481
Rodrigo <rodrigo.facury14@gmail.com>
2022-02-01 18:54:36 -0300

    chore: retira desestruturaçao do req.body e do newSaleInfo

--- back-end/src/controllers/sales/sales.js
+++ back-end/src/controllers/sales/sales.js
@@ -2,17 +2,14 @@ const serviceSales = require('../../services/sales');
 
 const createSale = async (req, res, next) => {
   try {
-    const { products, totalPrice, deliveryAddress, deliveryNumber, userId, sellerId,
-    } = req.body;
+    const sale = req.body;
 
-    const result = await serviceSales({
-      products,
-      totalPrice,
-      deliveryAddress,
-      deliveryNumber,
-      userId,
-      sellerId,
-    });
+    const result = await serviceSales(sale);
+
+    if (result.message) {
+      next(result);
+    }
+
     return res.status(201).json(result);
   } catch (error) {
     console.log(error);
--- back-end/src/services/sales.js
+++ back-end/src/services/sales.js
@@ -1,20 +1,14 @@
 const { sale, salesProduct } = require('../database/models');
 
 const sales = async (newsaleInfo) => {
-  const {
-    products, totalPrice, deliveryAddress, deliveryNumber, userId, sellerId,
-  } = newsaleInfo;
+  const { products } = newsaleInfo;
 
-  const newSale = await sale.create({
-    totalPrice, deliveryAddress, deliveryNumber, userId, sellerId,
-  });
-
-  const { id: saleId } = newSale;
+  const newSale = await sale.create(newsaleInfo);
 
   await products.forEach(async (product) => {
     await salesProduct.create({
       productId: product.id,
-      saleId,
+      saleId: newSale.id,
       quantity: product.quantity,
     });
   });
