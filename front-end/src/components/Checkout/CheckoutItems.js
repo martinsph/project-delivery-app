572bdeb61bf09bee4cf946e51ef2c908090bce0f
Alan Gonçalves <vekio.soft@gmail.com>
2022-02-01 18:23:28 -0300

    configura página de checkout buscando produtos do carrinho

--- front-end/src/components/Checkout/CheckoutItems.js
+++ front-end/src/components/Checkout/CheckoutItems.js
@@ -32,7 +32,7 @@ export const Td = styled.td`
 `;
 
 export const Table = styled.table`
-  border-collapse: collapse;
+
 `;
 
 export const Th = styled.th`
@@ -40,9 +40,36 @@ export const Th = styled.th`
 `;
 
 function CheckoutItems() {
+  // data mock
+  const payload = [
+    {
+      product: 'Heineken',
+      quantity: 5,
+      unitPrice: 'R$ 5.69',
+      subTotal: 'R$ 28.45',
+    },
+    {
+      product: 'Heineken',
+      quantity: 5,
+      unitPrice: 'R$ 5.69',
+      subTotal: 'R$ 28.45',
+    },
+    {
+      product: 'Heineken',
+      quantity: 5,
+      unitPrice: 'R$ 5.69',
+      subTotal: 'R$ 28.45',
+    },
+  ];
+
+  // mimicking product page
+  localStorage.setItem('cart', JSON.stringify(payload));
+  // getting cart products
+  const cart = JSON.parse(localStorage.getItem('cart'));
+
   return (
     <Table>
-      <tbody>
+      <thead>
         <tr>
           <Th>Item</Th>
           <Th>Descrição</Th>
@@ -51,26 +78,47 @@ function CheckoutItems() {
           <Th>Sub-total</Th>
           <Th>Remover Item</Th>
         </tr>
-      </tbody>
+      </thead>
       <tbody>
-        <tr>
-          <Td>2</Td>
-          <Td>Cerveja Heineken 330ml</Td>
-          <Td>5</Td>
-          <Td>R$7,90</Td>
-          <Td>R$15,80</Td>
-          <Td>Remover</Td>
-        </tr>
-      </tbody>
-      <tbody>
-        <tr>
-          <Td>2</Td>
-          <Td>Cerveja Heineken 330ml</Td>
-          <Td>5</Td>
-          <Td>R$7,90</Td>
-          <Td>R$15,80</Td>
-          <Td>Remover</Td>
-        </tr>
+        {
+          cart.map((items, i) => {
+            const { product, quantity, unitPrice, subTotal } = items;
+            return (
+              <tr key={ i }>
+                <Td
+                  data-testid="customer_checkout__element-order-table-item-number-"
+                >
+                  { i + 1 }
+                </Td>
+                <Td
+                  data-testid="customer_checkout__element-order-table-name-"
+                >
+                  { product }
+                </Td>
+                <Td
+                  data-testid="customer_checkout__element-order-table-quantity-"
+                >
+                  { quantity }
+                </Td>
+                <Td
+                  data-testid="customer_checkout__element-order-table-unit-price-"
+                >
+                  { unitPrice }
+                </Td>
+                <Td
+                  data-testid="customer_checkout__element-order-table-sub-total-"
+                >
+                  { subTotal }
+                </Td>
+                <Td
+                  data-testid="customer_checkout__element-order-table-remove-"
+                >
+                  Remover
+                </Td>
+              </tr>
+            );
+          })
+        }
       </tbody>
     </Table>
   );
