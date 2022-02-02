80252d7a2937557d5d24f22067e415e96b9726f1
martinsph <ph.mmartins@gmail.com>
2022-02-02 17:56:14 -0300

    feat: implementaçao do getSalaeById na pagina de detalhes do pedido

--- back-end/src/controllers/sales/sales.js
+++ back-end/src/controllers/sales/sales.js
@@ -4,7 +4,7 @@ const createSale = async (req, res, next) => {
   try {
     const sale = req.body;
 
-    const result = await serviceSales(sale);
+    const result = await serviceSales.createSale(sale);
 
     return res.status(201).json(result);
   } catch (error) {
@@ -12,4 +12,16 @@ const createSale = async (req, res, next) => {
   }
 };
 
-module.exports = { createSale };
+const getSalebyId = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const result = await serviceSales.getSaleById(id);
+    return res.status(200).json(result);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+module.exports = { createSale, getSalebyId };
